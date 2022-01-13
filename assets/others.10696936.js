981f1c5066a0e396e50d4cfc90b3ff8e1e0db1ec
Morpho <samuiyuki@outlook.com>
2022-01-13 15:12:45 +0800

    Updates

--- assets/others.10696936.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./index.299bc7d5.js";function s(s){return r({url:"/search",method:"get",params:s})}export{s};
---
+++ assets/others.2db637bd.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index.a3cf159d.js";function s(s){return r({url:"/search",method:"get",params:s})}export{s};
