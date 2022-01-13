10e56ec106da39df084ff628532ef51908cd51e7
Morpho <samuiyuki@outlook.com>
2022-01-13 16:08:59 +0800

    Updates

--- assets/others.2db637bd.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./index.a3cf159d.js";function s(s){return r({url:"/search",method:"get",params:s})}export{s};
---
+++ assets/others.ae058294.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index.f8b6e220.js";function e(e){return r({url:"/search",method:"get",params:e})}export{e as s};
